fb14bebb075e0d6fdc976caf73d11e6a1bd70e3e
bhargavchintha <bhargavchintha5588@gmail.com>
2024-03-18 18:59:53 +0530

    ok

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,100 +1,112 @@
-import React, { Component }  from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passwordType: 'password'
+    };
+  }
+
+  showPassword = () => {
+    this.setState(prevState => ({
+      passwordType: prevState.passwordType === 'password' ? 'text' : 'password'
+    }));
+  };
 
   render() {
-    
+    const { passwordType } = this.state;
     return (
       <>
-       <div className='Login_Page_Full'>
-            <title>Sign Up Page</title>
+        <div className='Login_Page_Full'>
+          <title>Sign Up Page</title>
           <div className='Login_page'>
-            
+
             <div className='Dark_Btn_Logo'>
-                <div className='Logo_Music'>
-                  {/* <Link className="Music_Name-Ex" to="/Home">Music Explore</Link> */}
-                </div>
-                <div className='Dark_Btn_Switch'>
+              <div className='Logo_Music'>
+                {/* <Link className="Music_Name-Ex" to="/Home">Music Explore</Link> */}
+              </div>
+              <div className='Dark_Btn_Switch'>
                 <Link className="Music_Name-Ex" to="/Home">Music Explore</Link>
-                </div>
+              </div>
             </div>
-        
-          <div className='Login_From_Sign'>
-            <div className='Login_From_Create'>
-                <form className='Login_Page' id='loginForm' method='post' action='' >
+
+            <div className='Login_From_Sign'>
+              <div className='Login_From_Create'>
+                <form className='Login_Page' id='loginForm' method='post' action=''>
                   <h1 className='Name_Sign_e3'>
-                  Sign up to start  <span className='Music_Name'><br></br> listening</span>
+                    Sign up to start <span className='Music_Name'><br></br> listening</span>
                   </h1>
                   <div className='Name-User_Field'>
-                    <div class="input-field">
-                      <input type="text" required  name='name' /> 
-                      <label  className='Enter_Email' >Name</label>
-                  </div>
-                </div>
-                <div className='Name-User_Field'>
-                    <div class="input-field">
-                      <input type="text" required  name='Userid' /> 
-                      <label  className='Enter_Email' >user Id</label>
-                  </div>
-                </div>
-                <div className='Email-User_Field'>
-                  <div class="input-field">
-                    <input type="email" required  name='email' /> 
-                    <label  className='Enter_Email' >Enter email</label>
-                  </div>
-                </div>
-                <div className='Cn_Passwords'>
-                  <div className='Password_Field'>
-                    <div class="input-field">
-                      <input  id="Showpassword" type='password' required  name='Password' /> 
-                      <label  className='Enter_Email' >Password</label>
+                    <div className="input-field">
+                      <input type="text" required name='name' />
+                      <label className='Enter_Email' >Name</label>
                     </div>
                   </div>
-                  <div className='Confirm_Password_Field'>
-                    <div class="input-field">
-                      <input  id="Showpassword" type='password' required  name='confirmpassword' /> 
-                      <label  className='Enter_Email' >confirm Password</label>
+                  <div className='Name-User_Field'>
+                    <div className="input-field">
+                      <input type="text" required name='Userid' />
+                      <label className='Enter_Email' >user Id</label>
                     </div>
                   </div>
-                  <div className='Error_msg'>
-                    <p className='Msg_error'></p>
+                  <div className='Email-User_Field'>
+                    <div className="input-field">
+                      <input type="email" required name='email' />
+                      <label className='Enter_Email' >Enter email</label>
+                    </div>
                   </div>
-                </div>
-                <div className='Password-Show'>
+                  <div className='Cn_Passwords'>
+                    <div className='Password_Field'>
+                      <div className="input-field">
+                        <input id="Showpassword" type={passwordType} required name='Password' />
+                        <label className='Enter_Email' >Password</label>
+                      </div>
+                    </div>
+                    <div className='Confirm_Password_Field'>
+                      <div className="input-field">
+                        <input id="Showpassword" type={passwordType} required name='confirmpassword' />
+                        <label className='Enter_Email' >confirm Password</label>
+                      </div>
+                    </div>
+                    <div className='Error_msg'>
+                      <p className='Msg_error'></p>
+                    </div>
+                  </div>
+                  <div className='Password-Show'>
 
-                  <div className='Show_password'>
-                    <input type="checkbox" id="showPassword"  /><label htmlFor="showPassword" className="Show_pass">Show Password</label>
+                    <div className='Show_password'>
+                      <input type="checkbox" id="showPassword" onClick={this.showPassword} /><label  className="Show_pass">Show Password</label>
+                    </div>
+
+
                   </div>
 
-
-                </div>
-
-                <div className='Submit_Button_Sign'>
-                  <input  type='submit' value={'Submit'}/>
-                </div>
+                  <div className='Submit_Button_Sign'>
+                    <input type='submit' value={'Submit'} />
+                  </div>
 
                 </form>
 
-              <div className='Hr_Line'>
-                <div className='Hr_Line_txt'>
+                <div className='Hr_Line'>
+                  <div className='Hr_Line_txt'>
                     <p className='Hr_Line_Er'>or</p>
-                </div>
-                  <div className='Sign_up-Page'>
-                  <p className='Link_send'> Already have an account? <Link className='Link_Sign-Up' to='/Login'>Login Here</Link></p>
                   </div>
+                  <div className='Sign_up-Page'>
+                    <p className='Link_send'> Already have an account? <Link className='Link_Sign-Up' to='/Login'>Login</Link></p>
+                  </div>
+                </div>
+
               </div>
-
             </div>
-          </div>
 
 
           </div>
-       </div>
-    
-    </>
+        </div>
+
+      </>
     )
   }
 }
 
-export default Signup
+export default Signup;
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(prevState => !prevState);
   };
-
+  
   return (
     <>
       <nav className='Music_Explore'>
@@ -74,7 +74,7 @@ const Navbar = () => {
           <div className='Music_Search'>
             <form action="" class="search-bar">
                 <i id="Searchbar" class="fa fa-search" aria-hidden="true"></i>
-                <input type="search" name="search" pattern=".*\S.*" required autoComplete="off" />
+                <input type="search" name="search" pattern=".*\S.*" required autoComplete="off" onClick={handleClick} />
             </form>
           </div>
          </div>
@@ -87,7 +87,7 @@ const Navbar = () => {
               <div className='Music_Search'>
                   <form action="" class="search-bar">
                       <i id="Searchbar" class="fa fa-search" aria-hidden="true"></i>
-                      <input type="search" name="search" pattern=".*\S.*" required  autoComplete="off" />
+                      <input type="search" name="search" pattern=".*\S.*" required  autoComplete="off" onClick={handleClick} />
                   </form>
               </div>
             </div>
@@ -96,41 +96,42 @@ const Navbar = () => {
                     <i className={`fa ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
                 </label>
                   <input id="menuTrigger" type="checkbox"  class="nav_I" />
-            <div className='Bars_Link_Music'>
-              <div className='Button_Dark_Mode'>
-                  <button onClick={toggleDarkMode} id="Pos_Btn" className="BUTNS Button_BTN" title="Change To Dark Mode"></button>
-              </div>
-              <div className='Home_Search'>
-                <div className='Down_Home'>
-                     <Link  className="Home_name" to="/Home" >
-                     <i id="Fa_Home_B" class="fa fa-home" aria-hidden="true"></i> Home</Link>
-                </div>
-                <div className='In_Side_Search'>
-                  <div className='Down_Home'>
-                      <Link  className="Home_name" to="/Search" >
-                      <i id="Fa_Home_B" class="fa fa-search" aria-hidden="true"></i> Search</Link>
-                  </div>
-                </div>
-                <div className='File_Upload'>
-                   <button className='BTN_Upload' onClick={handleClick} > <i id="Fa-Upload" class="fa fa-upload" aria-hidden="true"></i>  Music Upload </button>
-                </div>
-                <div className='Search_btn'>
-                  <div className='Search_All'>
-                      <Link to='/About' className='Btn_Search'> <i id="Info_Cricle" class="fa fa-info-circle" aria-hidden="true"></i> About</Link>
-                  </div>
-                </div>
-                <div className='Down_Home'>
-                     <Link  className="Home_name" to="/Signup" >
-                     <i id="Fa_Home_B" class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;Sign up</Link>
-                </div>
-                <div className='Down_Home'>
-                     <Link  className="Home_name" to="/Login" >
-                     <i id="Fa_Home_B" class="fa fa-user" aria-hidden="true"></i> Login</Link>
-                </div>
+            <div className='Bars_Link_Music' >
+                    <div className='Button_Dark_Mode'>
+                        <button onClick={toggleDarkMode} id="Pos_Btn" className="BUTNS Button_BTN" title="Change To Dark Mode"></button>
+                    </div>
+                  <div className='Home_Search'>
+                    <div className='Down_Home'>
+                    <Link className="Home_name" to="/Home" >
+                        <i id="Fa_Home_B" class="fa fa-home" aria-hidden="true"></i> Home</Link>
+                    </div>
+                    <div className='In_Side_Search'>
+                      <div className='Down_Home'>
+                          <Link  className="Home_name" to="/Search" onClick={handleClick} >
+                          <i id="Fa_Home_B" class="fa fa-search" aria-hidden="true"></i> Search</Link>
+                      </div>
+                    </div>
+                    <div className='File_Upload'>
+                      <button className='BTN_Upload' onClick={handleClick} > <i id="Fa-Upload" class="fa fa-upload" aria-hidden="true"></i>  Music Upload </button>
+                    </div>
+                    <div className='Search_btn'>
+                      <div className='Search_All'>
+                          <Link to='/About' className='Btn_Search'> <i id="Info_Cricle" class="fa fa-info-circle" aria-hidden="true"></i> About</Link>
+                      </div>
+                    </div>
+                    <div className='Down_Home'>
+                        <Link  className="Home_name" to="/Signup" >
+                        <i id="Fa_Home_B" class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;Sign up</Link>
+                    </div>
+                    <div className='Down_Home'>
+                        <Link  className="Home_name" to="/Login" >
+                        <i id="Fa_Home_B" class="fa fa-user" aria-hidden="true"></i> Login</Link>
+                    </div>
 
-              </div>
-            
-              <div className='Sign_up-Login'>
+                  </div>
+            </div>
+
+            <div className='Sign_up-Login'>
                   <div className='Nav_Bar_sign'>
                       <Link className="Sign_Login  SignUp" to="/Signup">Sign up</Link>
                   </div>
@@ -140,7 +141,7 @@ const Navbar = () => {
 
                     <button onClick={toggleDarkMode} className="BUTNS Button_BTN" title="Change To Dark Mode"></button>
               </div>
-            </div>
+
           </div>
         </div>
         
@@ -149,6 +150,6 @@ const Navbar = () => {
       {/* <img src='https://i.stack.imgur.com/jDGzD.png'/> */}
     </>
   );
-}
+};
 
 export default Navbar;
